8623e7876477335ba87ddb9551e10a2b193b340c
Dmitriy Oboznyi <DObozniy@gmail.com>
2019-04-20 17:24:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python
-from redis import Redis, RedisError
+import sys
 import os
 import socket
+from redis import Redis, RedisError
 
-# Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
-if __name__ == "__main__":
+def application(environ, start_response):
     try:
         visits = redis.incr("counter")
     except RedisError:
@@ -15,4 +14,12 @@ if __name__ == "__main__":
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-    print(html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits))
+    status = '200 OK'
+    output = bytes(html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits), 'utf-8')
+
+    response_headers = [('Content-type', 'text/plain'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
+
